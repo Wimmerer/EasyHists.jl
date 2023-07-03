3903a759e20091b7b2bb486703b16cf5c1863dc0
Documenter.jl <documenter@juliadocs.github.io>
2023-07-03 15:49:20 +0000

    build based on f7c1603

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EasyHists","category":"page"},{"location":"#EasyHists","page":"Home","title":"EasyHists","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EasyHists.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EasyHists]","category":"page"}]
+}
